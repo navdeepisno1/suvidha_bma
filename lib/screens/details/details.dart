76b3d36a3aeae7c4bdb365df82f8d6e0db0ea79d
Navdeep Sharma <navdeepisno1@gmail.com>
2021-06-21 08:26:45 +0530

    Added Animations to home and details page

--- lib/screens/details/details.dart
+++ lib/screens/details/details.dart
@@ -9,6 +9,7 @@ import 'package:business_finance_management/utils/widgets/custom_text.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class DetailsScreen extends StatefulWidget {
   const DetailsScreen({key}) : super(key: key);
@@ -20,128 +21,143 @@ class DetailsScreen extends StatefulWidget {
 class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    List<Widget> bodyWidgets = [
+      HeadingWithSubheading(),
+      CustomTab(),
+      Center(
+        child: CustomText(
+          'Rs. 12,100',
+          fontSize: 24,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.check,
+              color: kColorGrey,
+            ),
+            CustomText(
+              '8.4% increase',
+              fontWeight: FontWeight.bold,
+              color: kColorGrey,
+            ),
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.only(right: kAppPadding),
+        height: 250,
+        child: BarChart(
+          BarChartData(
+            borderData: FlBorderData(
+              show: true,
+              border: Border(
+                bottom: BorderSide(color: kColorGrey, width: 2),
+              ),
+            ),
+            barGroups: List.generate(
+              6,
+              (index) => BarChartGroupData(
+                x: index + 17,
+                barRods: [
+                  BarChartRodData(
+                      y: Random().nextInt(10).toDouble() + 5,
+                      width: 12,
+                      colors: [kColorPrimary]),
+                  BarChartRodData(
+                      y: Random().nextInt(10).toDouble() + 5,
+                      width: 12,
+                      colors: [kColorTertiary]),
+                ],
+              ),
+            ),
+            maxY: 15,
+            minY: 0,
+          ),
+          swapAnimationDuration: Duration(milliseconds: 150),
+          swapAnimationCurve: Curves.linear,
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: kAppPadding,
+          vertical: kAppPadding / 2,
+        ),
+        height: 250,
+        decoration: BoxDecoration(
+          color: kColorTertiary2,
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg_3.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.circular(kBorderRadius),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            CustomText(
+              'Tips and tricks for business',
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+              color: kColorWhite,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: kAppPadding,
+              ),
+              padding: EdgeInsets.symmetric(vertical: 8),
+              decoration: BoxDecoration(
+                color: kColorWhite,
+                borderRadius: BorderRadius.circular(kBorderRadius),
+                boxShadow: [
+                  BoxShadow(
+                    color: kColorBlack.withOpacity(0.1),
+                    blurRadius: 12,
+                  ),
+                ],
+              ),
+              child: CustomText(
+                'Click Me !!',
+                color: kColorGrey,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+      SizedBox(
+        height: 84,
+      ),
+    ];
+
     return Scaffold(
       body: SafeArea(
         child: Container(
           child: Stack(
             children: [
               Container(
-                child: ListView(
-                  children: [
-                    HeadingWithSubheading(),
-                    CustomTab(),
-                    Center(
-                      child: CustomText(
-                        'Rs. 12,100',
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.check,
-                            color: kColorGrey,
+                child: AnimationLimiter(
+                  child: ListView.builder(
+                    itemCount: bodyWidgets.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return AnimationConfiguration.staggeredList(
+                        position: index,
+                        duration: const Duration(milliseconds: 375),
+                        child: SlideAnimation(
+                          verticalOffset: 50.0,
+                          child: FadeInAnimation(
+                            child: bodyWidgets[index],
                           ),
-                          CustomText(
-                            '8.4% increase',
-                            fontWeight: FontWeight.bold,
-                            color: kColorGrey,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(right: kAppPadding),
-                      height: 250,
-                      child: BarChart(
-                        BarChartData(
-                          borderData: FlBorderData(
-                            show: true,
-                            border: Border(
-                              bottom: BorderSide(color: kColorGrey, width: 2),
-                            ),
-                          ),
-                          barGroups: List.generate(
-                            6,
-                            (index) => BarChartGroupData(
-                              x: index + 17,
-                              barRods: [
-                                BarChartRodData(
-                                    y: Random().nextInt(10).toDouble() + 5,
-                                    width: 12,
-                                    colors: [kColorPrimary]),
-                                BarChartRodData(
-                                    y: Random().nextInt(10).toDouble() + 5,
-                                    width: 12,
-                                    colors: [kColorTertiary]),
-                              ],
-                            ),
-                          ),
-                          maxY: 15,
-                          minY: 0,
                         ),
-                        swapAnimationDuration: Duration(milliseconds: 150),
-                        swapAnimationCurve: Curves.linear,
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: kAppPadding,
-                        vertical: kAppPadding / 2,
-                      ),
-                      height: 250,
-                      decoration: BoxDecoration(
-                        color: kColorTertiary2,
-                        image: DecorationImage(
-                          image: AssetImage('assets/images/bg_3.png'),
-                          fit: BoxFit.cover,
-                        ),
-                        borderRadius: BorderRadius.circular(kBorderRadius),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          CustomText(
-                            'Tips and tricks for business',
-                            fontSize: 24,
-                            fontWeight: FontWeight.w700,
-                            color: kColorWhite,
-                          ),
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                              horizontal: kAppPadding,
-                            ),
-                            padding: EdgeInsets.symmetric(vertical: 8),
-                            decoration: BoxDecoration(
-                              color: kColorWhite,
-                              borderRadius:
-                                  BorderRadius.circular(kBorderRadius),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: kColorBlack.withOpacity(0.1),
-                                  blurRadius: 12,
-                                ),
-                              ],
-                            ),
-                            child: CustomText(
-                              'Click Me !!',
-                              color: kColorGrey,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 84,
-                    ),
-                  ],
+                      );
+                    },
+                  ),
                 ),
               ),
               Positioned(
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,6 +7,7 @@ import 'package:business_finance_management/utils/widgets/app_bottom_navbar.dart
 import 'package:business_finance_management/utils/widgets/custom_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({key}) : super(key: key);
@@ -18,120 +19,130 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    List<Widget> bodyWidgets = [
+      HeadingWithSubheading(),
+      CustomTab(),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height: 350,
+        margin: EdgeInsets.symmetric(horizontal: kAppPadding, vertical: 12),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 250,
+                decoration: BoxDecoration(
+                  color: kColorTertiary,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 56,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/bg_2.png',
+                      width: MediaQuery.of(context).size.width * 0.55,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 32,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: kAppPadding),
+                decoration: BoxDecoration(
+                  color: kColorWhite,
+                  borderRadius: BorderRadius.circular(kBorderRadius),
+                ),
+                child: ListTile(
+                  title: CustomText(
+                    'Balance',
+                    fontWeight: FontWeight.bold,
+                  ),
+                  subtitle: CustomText(
+                    'Rs. 12,100',
+                  ),
+                  trailing: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DetailsScreen()));
+                    },
+                    child: CircleAvatar(
+                      radius: 32,
+                      backgroundColor: kColorSecondary,
+                      child: Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: kColorPrimary,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      Column(
+        children: List.generate(
+          10,
+          (index) => Container(
+            margin: EdgeInsets.symmetric(vertical: 12),
+            child: ListTile(
+              contentPadding: EdgeInsets.symmetric(horizontal: kAppPadding),
+              leading: Image.asset(
+                'assets/icons/icon_send.png',
+              ),
+              title: CustomText(
+                'Title',
+                fontWeight: FontWeight.w700,
+              ),
+              subtitle: CustomText('Subtitle'),
+            ),
+          ),
+        ),
+      ),
+      SizedBox(
+        height: 56,
+      ),
+    ];
+
     return Scaffold(
       body: SafeArea(
         child: Container(
           child: Stack(
             children: [
               Container(
-                child: ListView(
-                  children: [
-                    HeadingWithSubheading(),
-                    CustomTab(),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 350,
-                      margin: EdgeInsets.symmetric(
-                          horizontal: kAppPadding, vertical: 12),
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            bottom: 0,
-                            left: 0,
-                            right: 0,
-                            child: Container(
-                              width: MediaQuery.of(context).size.width,
-                              height: 250,
-                              decoration: BoxDecoration(
-                                color: kColorTertiary,
-                                borderRadius: BorderRadius.circular(16),
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            left: 0,
-                            right: 0,
-                            bottom: 56,
-                            child: Container(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image.asset(
-                                    'assets/images/bg_2.png',
-                                    width: MediaQuery.of(context).size.width *
-                                        0.55,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            left: 0,
-                            right: 0,
-                            bottom: 32,
-                            child: Container(
-                              margin:
-                                  EdgeInsets.symmetric(horizontal: kAppPadding),
-                              decoration: BoxDecoration(
-                                color: kColorWhite,
-                                borderRadius:
-                                    BorderRadius.circular(kBorderRadius),
-                              ),
-                              child: ListTile(
-                                title: CustomText(
-                                  'Balance',
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                subtitle: CustomText(
-                                  'Rs. 12,100',
-                                ),
-                                trailing: GestureDetector(
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                DetailsScreen()));
-                                  },
-                                  child: CircleAvatar(
-                                    radius: 32,
-                                    backgroundColor: kColorSecondary,
-                                    child: Icon(
-                                      Icons.arrow_forward_ios_rounded,
-                                      color: kColorPrimary,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Column(
-                      children: List.generate(
-                        10,
-                        (index) => Container(
-                          margin: EdgeInsets.symmetric(vertical: 12),
-                          child: ListTile(
-                            contentPadding:
-                                EdgeInsets.symmetric(horizontal: kAppPadding),
-                            leading: Image.asset(
-                              'assets/icons/icon_send.png',
-                            ),
-                            title: CustomText(
-                              'Title',
-                              fontWeight: FontWeight.w700,
-                            ),
-                            subtitle: CustomText('Subtitle'),
+                child: AnimationLimiter(
+                  child: ListView.builder(
+                    itemCount: bodyWidgets.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return AnimationConfiguration.staggeredList(
+                        position: index,
+                        duration: const Duration(milliseconds: 375),
+                        child: SlideAnimation(
+                          verticalOffset: 50.0,
+                          child: FadeInAnimation(
+                            child: bodyWidgets[index],
                           ),
                         ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 56,
-                    ),
-                  ],
+                      );
+                    },
+                  ),
                 ),
               ),
               Positioned(
